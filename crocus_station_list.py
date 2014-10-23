bc3cd7398414ccadfc29dcf01778852dd8a24057
Müller Karsten <kmu@h-fou02>
2014-10-23 16:11:10 +0200

    Started working on the Crocus station list.

---
+++ crocus_station_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""
+Author. kmunve
+"""
+from lxml import etree
+from wsklima_getdata import wsKlimaRequest
+
+
+wr = wsKlimaRequest('getStationsFromTimeserieTypeElemCodes', {'timeserietypeID': 2, 'elem_codes': ['RR_1', 'RR_24', 'TA', 'UU', 'FF'], 'use\
+rname': ""}).get()
+
+
+# Parse XML string
+root = etree.fromstring(wr.content)
+
+# Temporary list of element codes
+station_list = []
+
+# Iterate over all "item" elements
+for element in root.iter("item"):
+    #        fid.write("{0},{1},{2},{3},{4},{5}\n".format(element.find('elemGroup').text.encode('utf-8'), element.find('name').text.encode('utf-8')element.find('elemCode').text.encode('utf-8'), element.find('unit').text.encode('utf-8'), element.find('elemNo').text.encode('utf-8'), element.find('description').text.encode('utf-8')))
+
+    station_list.append(int(element.find('stnr').text))
+
+print "Found {0} stations.".format(len(station_list))
+print station_list
