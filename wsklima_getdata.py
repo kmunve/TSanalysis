53899351392f8ecbb45ebe171c5fd4f892714bcd
kmunve <kmu@nve.no>
2015-08-21 10:46:35 +0200

    changed /usr/bin/python to /usr/bin/env python

--- wsklima_getdata.py
+++ wsklima_getdata.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 Author: kmunve
 """
@@ -11,9 +11,9 @@ class wsKlimaRequest():
 
     Usage: see "test_..." functions at the end of this file.
 
-    """    
+    """
     def __init__(self, method, params=None):
-        
+
         self.wsmethod = method
         self.wsmethods = ['getElementsProperties', 'getElementsFromTimeserieType', 'getStationsFromTimeserieTypeElemCodes', 'getStationsFromTimeserieTypeStationsElemCode', 'getMetData', 'getMetDataValue']
 
@@ -28,7 +28,7 @@ class wsKlimaRequest():
         self.wsklima_url = self.base_url
 
         self._create_wsklima_url()
-        
+
 
     def _create_wsklima_url(self):
 
@@ -37,7 +37,7 @@ class wsKlimaRequest():
 
         elif self.wsmethod == 'getMetDataValue':
             self.wsklima_url = "{0}?invoke=getMetData&timeserietypeID={1}&format={2}&from={3}&to={4}&stations={5}&elements={6}&hours={7}&months={8}&username={9}".format(self.base_url, self.params['timeserietypeID'], self.params['format'], self.params['from'], self.params['to'], ",".join(map(str, self.params['stations'])), ",".join(map(str, self.params['elements'])), ",".join(map(str, self.params['hours'])), self.params['months'], self.params['username'])
-        
+
         elif self.wsmethod == 'getStationsFromTimeserieTypeElemCodes':
             # Use this method if at least one of the elements in elem_codes should be measured. If all elements should be measured at the station use 'getStationsFromTimeserieTypeStationsElemCode'.
             # params = timeserietypeID, elem_codes, username
@@ -47,11 +47,11 @@ class wsKlimaRequest():
             # Use this method if all elements in elem_codes should be measured. If at least one should be measured at the station use 'getStationsFromTimeserieTypeElemCodes'.
             # params = timeserietypeID, elem_codes, username, stations
             self.wsklima_url = "{0}?invoke=getStationsFromTimeserieTypeStationsElemCode&timeserietypeID={1}&stations={4}&elem_codes={2}&username={3}".format(self.base_url, self.params['timeserietypeID'], ",".join(map(str, self.params['elem_codes'])), self.params['username'], ",".join(map(str, self.params['stations'])))
-            
+
         elif self.wsmethod == 'getElementsFromTimeserieType':
             # params = timeserietypeID
             self.wsklima_url = "{0}?invoke=getElementsFromTimeserieType&timeserietypeID={1}".format(self.base_url, self.params['timeserietypeID'])
-            
+
         elif self.wsmethod == 'getElementsProperties':
             # params = language, elem_codes
             self.wsklima_url = "{0}?invoke=getElementsProperties&language={1}&elem_codes={2}".format(self.base_url, self.params['language'], ",".join(map(str, self.params['elem_codes'])))
@@ -63,7 +63,7 @@ class wsKlimaRequest():
 
 
     def get(self):
-        ### Returns a requests.Response object                                                                      
+        ### Returns a requests.Response object
         return requests.get(self.wsklima_url)
 
 
@@ -100,7 +100,7 @@ class wsKlimaRequest():
 
         else:
             print "No valid method given!"
-    
+
 def test_getStationsFromTimeserieTypeElemCodes():
     wr = wsKlimaRequest('getStationsFromTimeserieTypeElemCodes', {'timeserietypeID': 2, 'elem_codes': ['RR_1', 'RR_24', 'TA', 'UU', 'FF'], 'username': ""}).get()
     print wr.text
@@ -112,29 +112,35 @@ def test_getStationsFromTimeserieTypeStationsElemCode():
     rsp = wr.get()              # get the requests.Response
     print rsp.text
     print rsp.url
-            
-    
-def test_getMetData():
+
+
+def test_getMetData(save=False):
     wr = wsKlimaRequest('getMetData', {'timeserietypeID': 2, 'format': "", 'from': '2014-01-01', 'to': '2014-01-31', 'stations': [54110,], 'elements': ['TA', 'RR_1', 'RR_24', 'FF', 'UU'], 'hours': range(0,24), 'months': "", 'username': ""}).get()
     print wr.text
+    if save:
+        fname = '54110.xml'
+        _f = open(fname, 'w')
+        _f.write(wr.text)
+        _f.close()
+        print 'Data written to %s' % fname
     print wr.url
 
 # return XSD formated scheme
 def test_getMetDataValue():
     wr = wsKlimaRequest('getMetDataValue', {'timeserietypeID': 2, 'format': "", 'from': '2014-01-01', 'to': '2014-01-03', 'stations': [54110,], 'elements': ['TA', 'RR_1', 'RR_24', 'FF', 'UU'], 'hours': range(0,24), 'months': "", 'username': ""}).get()
     return wr
-    
-    
+
+
 def test_getElementsFromTimeserieType():
     wr = wsKlimaRequest('getElementsFromTimeserieType', {'timeserietypeID': 2}).get()
     print wr.text
     print wr.url
 
- 
+
 
 
 if __name__ == "__main__":
 #    test_getStationsFromTimeserieTypeElemCodes()
 #    test_getElementsFromTimeserieType()
-    test_getMetData()
+    test_getMetData(save=True)
 #    test_getStationsFromTimeserieTypeStationsElemCode()
