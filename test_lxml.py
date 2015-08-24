74c221eb82c6b8e1d579bfd41497dd4580630c77
kmunve <kmu@nve.no>
2015-08-24 15:08:52 +0200

    switched to using XPath in test_lxml.py

--- test_lxml.py
+++ test_lxml.py
@@ -29,11 +29,33 @@ tree = etree.parse(open('54110.xml'))
 
 root = tree.getroot()
 
-# Print tree structure of file
-print etree.tostring(root, pretty_print=True)
 
+rootTS = root.xpath('//timeStamp')
+print len(rootTS), rootTS[0].tag
+
+#TS = rootTS[0].xpath('item[@xsi:type="ns2:no_met_metdata_TimeStamp"]')
+TS = rootTS[0].xpath('item')
+for f in TS:
+    time = f.xpath('from')[0].text
+    L = f.xpath('location/item')
+    for loc in L:
+        stat_id = loc.xpath('id')[0].text
+        print stat_id
+        WEitems = loc.xpath('weatherElement/item')
+        for WE in WEitems:
+            we_name = WE.xpath('id')[0].text
+            we_q = WE.xpath('quality')[0].text
+            we_val = WE.xpath('value')[0].text
+            print we_name, we_q, we_val
+
+
+
+
+# Print tree structure of file
+#print etree.tostring(root, pretty_print=True)
+"""
 for child_of_root in root:
-    print child_of_root.tag, child_of_root.attrib
+    print child_of_root.tag, '---', child_of_root.attrib
 
 
 
@@ -53,12 +75,13 @@ for timestamp in tree.iter('timeStamp'):
 # Access each location-tag and store the station number in its id-tag
         loc = item.find('location')
         for locitem in loc:
-            """
+            '''
             Start to iterate over the items in the location tag
             see http://lxml.de/xpathxslt.html to get a better mapping.
-            """
+            '''
 
-            for stat_item in locitem.findall('item')
+            for stat_item in locitem.findall('item'):
+                pass
             # Get the station ID
 
             stat_id = locitem.find('id').text
@@ -69,26 +92,26 @@ for timestamp in tree.iter('timeStamp'):
             stat_id = np.int(stat_id)
 '''
 
-# Access the weatherElement
-            weather_e = locitem.find('weatherElement')
-            for weather_i in weather_e:
-# Access the parameter ID
-                param_id = weather_i.find('id').text
-                if param_id not in para_dict.keys():
-                    para_dict[param_id] = []
-# Access the quality tag
-                quality = weather_i.find('quality').text
-# Convert the quality tag to an interger
-                quality = np.int(quality)
-# Access the value
-                value = weather_i.find('value').text
-# Convert the value to float
-                value = np.float(value)
-# Print for testing
-                #print stat_id, tstamp, param_id, quality, value
+    # Access the weatherElement
+                weather_e = locitem.find('weatherElement')
+                for weather_i in weather_e:
+    # Access the parameter ID
+                    param_id = weather_i.find('id').text
+                    if param_id not in para_dict.keys():
+                        para_dict[param_id] = []
+    # Access the quality tag
+                    quality = weather_i.find('quality').text
+    # Convert the quality tag to an interger
+                    quality = np.int(quality)
+    # Access the value
+                    value = weather_i.find('value').text
+    # Convert the value to float
+                    value = np.float(value)
+    # Print for testing
+                    #print stat_id, tstamp, param_id, quality, value
 
-                para_dict[param_id].append(value)
-            stat_dict[stat_id] = para_dict
+                    para_dict[param_id].append(value)
+                stat_dict[stat_id] = para_dict
 
 
 '''
@@ -107,3 +130,5 @@ matplotlib.pyplot.gcf().savefig('12290.png', dpi=90)
 #populate_forcing_nc(df)
 
 #df.to_hdf('test_lxml.hdf', 'test_from_eklima')
+
+"""
