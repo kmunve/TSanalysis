53136655226138ae82ee7f2c93ffb6e06a877d12
kmunve <kmu@nve.no>
2015-09-02 14:35:06 +0200

    Created .gitignore

--- Test/test_xslt.py
+++ Test/test_xslt.py
@@ -9,8 +9,8 @@ import lxml.etree as ET
 
 #xml_filename = 'bookstore.xml'
 #xsl_filename = 'metdata.xsl'
-xml_filename = 'getStationsProperties.out.all.xml'
-xsl_filename = 'getStationsProperties.xsl'
+xml_filename = 'Data/getStationsProperties.out.all.xml'
+xsl_filename = 'Data/getStationsProperties.xsl'
 
 dom = ET.parse(xml_filename)
 xslt = ET.parse(xsl_filename)
