1743a9691f93f0bcf5d94bc66eaa6f16b10dc3dc
kmunve <kmu@nve.no>
2015-09-08 15:20:26 +0200

    get data directly from thredds; using masked array in ...stats..py

---
+++ Test/extract_thredds.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+# Orgiginal command from MET workshop
+#fimex --input.file=http://thredds.met.no/thredds/dodsC/arome25/arome_norway_default2_5km_20140123_06.nc --input.type=netcdf --extract.selectVariables=x_wind_10m --extract.selectVariables=y_wind_10m --extract.reduceTime.start=2014-01-23T10:00:00 --extract.reduceTime.end=2014-01-23T12:00:00 --extract.reduceToBoundingBox.south=59 --extract.reduceToBoundingBox.north=61 --extract.reduceToBoundingBox.east=11 --extract.reduceToBoundingBox.west=9 --output.file=out.nc --output.type=nc4
+
+# Extract bounding box around Rauland forecasting region
+fimex --input.file=http://thredds.met.no/thredds/dodsC/arome25/arome_metcoop_default2_5km_20150908_06.nc --input.type=netcdf \
+--extract.selectVariables=precipitation_amount \
+--extract.selectVariables=precipitation_amount_acc \
+--extract.selectVariables=precipitation_amount_high_estimate \
+--extract.selectVariables=precipitation_amount_low_estimate \
+--extract.selectVariables=precipitation_amount_middle_estimate \
+--extract.selectVariables=air_temperature_2m \
+--extract.selectVariables=land_area_fraction \
+--extract.selectVariables=altitude \
+--extract.reduceTime.start=2015-09-04T07:00:00 --extract.reduceTime.end=2015-09-05T06:00:00 \
+--extract.reduceToBoundingBox.south=59.55 --extract.reduceToBoundingBox.north=60.02 \
+--extract.reduceToBoundingBox.west=7.47 --extract.reduceToBoundingBox.east=9.02 \
+--output.file=Data/rauland_t.nc --output.type=nc4
+
+# Correct the extraction time - maybe make a python script that collects data daily.
+# Extract bounding box around Lofoten forecasting region
+fimex --input.file=http://thredds.met.no/thredds/dodsC/arome25/arome_metcoop_default2_5km_20150908_06.nc --input.type=netcdf \
+--extract.selectVariables=precipitation_amount \
+--extract.selectVariables=precipitation_amount_acc \
+--extract.selectVariables=precipitation_amount_high_estimate \
+--extract.selectVariables=precipitation_amount_low_estimate \
+--extract.selectVariables=precipitation_amount_middle_estimate \
+--extract.selectVariables=air_temperature_2m \
+--extract.selectVariables=land_area_fraction \
+--extract.selectVariables=altitude \
+--extract.reduceTime.start=2015-09-04T07:00:00 --extract.reduceTime.end=2015-09-05T06:00:00 \
+--extract.reduceToBoundingBox.south=67.82 --extract.reduceToBoundingBox.north=68.48 \
+--extract.reduceToBoundingBox.west=12.70 --extract.reduceToBoundingBox.east=15.57 \
+--output.file=Data/lofoten_t.nc --output.type=nc4
--- Test/thredds_stats_after_extract.py
+++ Test/thredds_stats_after_extract.py
@@ -28,11 +28,11 @@ def nc_info(nc_data):
 
 # Access netcdf file via OpenDAP
 #filename = 'http://thredds.met.no/thredds/dodsC/arome25/arome_metcoop_default2_5km_latest.nc'
-filename = './Data/rauland.nc'
+filename = './Data/rauland_t.nc'
 nc = netCDF4.Dataset(filename)
 
 # Get content
-nc_info(nc)
+#nc_info(nc)
 
 # Get coordinates and other standard variables
 x = nc.variables['x']
@@ -40,7 +40,13 @@ y = nc.variables['y']
 lat = nc.variables['latitude']
 lon = nc.variables['longitude']
 
-
+# Extract land area fraction and use as mask in coastal regions
+# 0 = 100% water cell, 1 = 100% land cell
+land_mask = np.ma.masked_equal(nc.variables['land_area_fraction'][:].squeeze(), 0)
+print(land_mask.mask)
+# Get number of non-masked elements
+land_cells = land_mask.count()
+print(land_cells)
 
 #ipdb.set_trace()
 
@@ -49,31 +55,52 @@ jd = netCDF4.num2date(times[:], times.units)
 
 # Extract specific data and remove unnecessary dimensions
 precip = nc.variables['precipitation_amount'][:].squeeze()
-precip_acc = nc.variables['precipitation_amount_acc'][:].squeeze()
+precip_low = nc.variables['precipitation_amount_low_estimate'][:].squeeze()
+precip_mid = nc.variables['precipitation_amount_middle_estimate'][:].squeeze()
+precip_high = nc.variables['precipitation_amount_high_estimate'][:].squeeze()
 
+# Double check if thats accumulates since model init
+# precip_acc = nc.variables['precipitation_amount_acc'][:].squeeze()
 
 #ipdb.set_trace()
 
 # sum up precip for 24 h after spin-up time
-precip_sum = np.sum(precip, axis=0)
+precip_sum = np.ma.array(np.sum(precip, axis=0), mask=land_mask.mask)
+precip_low_sum = np.ma.array(np.sum(precip_low, axis=0), mask=land_mask.mask)
+precip_mid_sum = np.ma.array(np.sum(precip_mid, axis=0), mask=land_mask.mask)
+precip_high_sum = np.ma.array(np.sum(precip_high, axis=0), mask=land_mask.mask)
 
-thresholds = {'0.01': 0., '5.0': 0., '10.0': 0., '20.0': 0., '40.0': 0.,}
+thresholds = {'1.0': 0., '5.0': 0., '10.0': 0., '20.0': 0., '40.0': 0.,}
+thresholds_low = thresholds.copy()
+thresholds_mid = thresholds.copy()
+thresholds_high = thresholds.copy()
+
 precip_thres = np.empty_like(thresholds)
 for t in thresholds.keys():
     _pt = np.where(precip_sum > np.float(t))
-    thresholds[t] = (np.float(_pt[0].size) / np.float(precip_sum.size))*100.
+    thresholds[t] = (np.float(_pt[0].size) / np.float(land_cells))*100.
 
-thresholds_acc = {'0.01': 0., '5.0': 0., '10.0': 0., '20.0': 0., '40.0': 0.,}
-precip_thres_acc = np.empty_like(thresholds)
-for t in thresholds_acc.keys():
-    _pt = np.where(precip_acc[-1,:,:] > np.float(t))
-    thresholds_acc[t] = (np.float(_pt[0].size) / np.float(precip_sum.size))*100.
+precip_thres_low = np.empty_like(thresholds_low)
+for t in thresholds_low.keys():
+    _pt = np.ma.masked_less_equal(precip_low_sum, np.float(t))
+    thresholds_low[t] = (np.float(_pt.count()) / np.float(land_cells))*100.
+
+precip_thres_mid = np.empty_like(thresholds_mid)
+for t in thresholds_mid.keys():
+    _pt = np.ma.masked_less_equal(precip_mid_sum, np.float(t))
+    thresholds_mid[t] = (np.float(_pt.count()) / np.float(land_cells))*100.
+
+precip_thres_high = np.empty_like(thresholds_high)
+for t in thresholds_high.keys():
+    _pt = np.ma.masked_less_equal(precip_high_sum, np.float(t))
+    thresholds_high[t] = (np.float(_pt.count()) / np.float(land_cells))*100.
 
 #ipdb.set_trace()
 
-plt.bar(np.array(thresholds.keys(), dtype=np.float), thresholds.values(), width=1.2, color='red')
+plt.bar(np.array(thresholds_high.keys(), dtype=np.float), thresholds_high.values(), width= 1.2, color='red')
 plt.hold(True)
-plt.bar(np.array(thresholds_acc.keys(), dtype=np.float), thresholds_acc.values())
+plt.bar(np.array(thresholds_mid.keys(), dtype=np.float), thresholds_mid.values(), width= 1.0, color='blue')
+plt.bar(np.array(thresholds_low.keys(), dtype=np.float), thresholds_low.values(), color='green')
 plt.figure()
-plt.imshow(precip_sum); plt.colorbar()
+plt.imshow(np.flipud(precip_sum)); plt.colorbar()
 plt.show()
