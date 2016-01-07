ec5eebd145d395c2d1ff9f14f501250d4eab76cb
kmunve <kmu@nve.no>
2016-01-07 15:42:30 +0100

    Ipython notebook for snow drift potential.

---
+++ Scripts/case_grotli.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+'''
+
+- download FORCING.nc from ftp://ftp.met.no/users/dagrunvs/soil/<today>
+- copy FORCING.nc to correct folder
+- run pgd/prep/offline
+- run snowpro.py
+- run period...
+- create vertical profile
+
+__author__ = 'kmu'
+'''
